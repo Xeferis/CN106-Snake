4edbd0e9029d760237eb5065035eed9a09f1602f
Flo <93609174+Xeferis@users.noreply.github.com>
2024-11-16 08:22:46 +0100

    Added tests

--- tests/value_tests.py
+++ tests/value_tests.py
@@ -5,6 +5,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from snake import *
 
 
+def test_zone():
+    z = zone(10, 10, 10, 10)
+    assert z.x == 10
+    assert z.y == 10
+    assert z.width == 10
+    assert z.height == 10
+
 def test_board():
     s = snake()
     b = board(snake=s)
@@ -12,8 +19,21 @@ def test_board():
     assert b.width == 500
     assert b.height == 500
     assert b.food == []
+    assert b.points2win == 100
+    assert b.start_x == 500 // 2
+    assert b.start_y == 500 // 2
+    assert b.level == 1
+    assert b.speed == 100
+    assert b.points == 0
+    assert b.won == False
     assert b.game_over == False
 
+def test_food():
+    f = food(10, 10)
+    assert f.x == 10
+    assert f.y == 10
+    assert f.height == 10
+    assert f.width == 10
 
 def test_snake():
     s = snake()
@@ -21,6 +41,8 @@ def test_snake():
     assert s.y == 0
     assert s.length == 0
     assert s.last == []
+    assert s.direction == None
+    assert s.tail == []
     assert s.width == 10
     assert s.height == 10
 
